bbf5622dfdae61d8af0b9a2519370617f18d9cbc
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2020-09-24 21:24:08 -0700

    Added total Bill Amount

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const billInput = document.querySelector('#bill')
 const tipInput = document.querySelector('#tip-input')
 const tipTotal = document.querySelector('#tip')
 const splitTip = document.querySelector('#people')
+const totalBill = document.querySelector('#total')
 
 billInput.value =  10
 splitTip.value = 2
@@ -14,9 +15,11 @@ function calculateTip(){
 
     const tipAmount = billValue * tipValue / 100
     const eachTip = tipAmount/ peopleValue
+    const totalAmount = billValue + tipAmount
 
     display.innerHTML = tipAmount.toFixed(2)
     tipTotal.innerHTML = eachTip.toFixed(2)
+    totalBill.innerHTML = totalAmount.toFixed(2)
 }
 
 calculateTip()
@@ -24,3 +27,4 @@ calculateTip()
 billInput.addEventListener('input', calculateTip)
 tipInput.addEventListener('input', calculateTip)
 splitTip.addEventListener('input',calculateTip)
+totalBill.addEventListener('listen', calculateTip)
